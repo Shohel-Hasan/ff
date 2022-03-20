14afd6e4b20d201b0d1aae9115c3f69a59e79a98
Nazirul-cloud <shanto2193@gmail.com>
2022-03-20 17:46:20 +0600

    icon toggle

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import style from '../styles/Footer.module.css';
 
 export default function Footer() {
     return (
-        <div className={`${style.main_container} container-fluid`}>
+        <div  className={`${style.main_container} container-fluid`}>
             <div className={`${style.footer_body} container d-flex flex-column`}>
                 <div className='d-flex mx-2'>Research Rider</div>
                 
@@ -12,11 +12,11 @@ export default function Footer() {
                 <div className='d-flex flex-wrap'>
                     <Link className='mx-2 my-1 text-decoration-none text-muted' to="/">Login</Link>
                     <Link className='mx-2 my-1 text-decoration-none text-muted' to="/user/:userId">Dashboard</Link>
-                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="/privacy-policy">Privacy Policy</Link>
-                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="/cookie-policy">Cookie Policy</Link>
-                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="/product-policy">Product Policy</Link>
-                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="/data-policy">Data Policy</Link>
-                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="/about-us">About Us</Link>
+                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="privacy-policy">Privacy Policy</Link>
+                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="cookie-policy">Cookie Policy</Link>
+                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="product-policy">Product Policy</Link>
+                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="data-policy">Data Policy</Link>
+                    <Link className='mx-2 my-1 text-decoration-none text-muted' to="about-us">About Us</Link>
                    
                 </div>
 
