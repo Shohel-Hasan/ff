1168ed9a42361f668c7ae5c3c0e43086422fb6d9
Nazirul-cloud <shanto2193@gmail.com>
2022-03-28 12:59:14 +0600

    wip

--- src/Pages/GroupProfile/GroupProfile.js
+++ src/Pages/GroupProfile/GroupProfile.js
@@ -482,7 +482,7 @@ useEffect(() => {
                 <div className="d-flex align-items-center">
                   <small style={{ color: "#1877f2", fontSize:'20px' }} > {singleGroup.name}  </small>
                   <div>
-                      <img className="badge-tag" src={badge} alt=''/>
+                      <img style={{'objectFit': 'cover' }} className="badge-tag m-1" src={badge} alt=''/>
                   </div>
                   <Modal  show={nameModal}  onHide={() => setNameModal(false)} dialogClassName="modal-90w" aria-labelledby="contained-modal-title-vcenter" centered>
                     <Modal.Header  closeButton >
